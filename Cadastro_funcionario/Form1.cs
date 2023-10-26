634ebed5813512cc3aefc999be88f1724909bf84
Ludimila <ludisantos3004@gmail.com>
2023-10-26 11:47:44 -0400

    cadastro funcionario

---
+++ Cadastro_funcionario/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadastro_funcionario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Funcionario funcionario = new Funcionario();
+                
+                string nome = nome_tx.Text;
+                string funcao = funcao_tx.Text;
+                string estado = estado_tx.Text;
+                string cidade = cidade_tx.Text;
+                string endereco = endereco_tx.Text;
+                string telefone = telefone_tx.Text;
+                string email = email_tx.Text;
+                string estadoCivil = estado_tx.Text;
+                double salario = Convert.ToDouble(salario_tx.Text);
+                string cpf = cpf_tx.Text;
+                string rg = rg_tx.Text;
+                DateTime datanascimento = Convert.ToDateTime(datanascimento_tx.Text);
+                MessageBox.Show(ValidarCpf.ValidaCPF(cpf).ToString());
+
+            }
+            catch(Exception ex)
+            { 
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nome_tx.Clear();
+            estado_tx.Clear();
+            cidade_tx.Clear();
+            endereco_tx.Clear();
+            telefone_tx.Clear();
+            email_tx.Clear();
+            estadocivil_tx.Clear();
+            salario_tx.Clear();
+            cpf_tx .Clear();
+            rg_tx .Clear();
+        }
+    }
+}
